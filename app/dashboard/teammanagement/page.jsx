da3b44ef3ccf77f298ed9140b418a95543a72b82
Bharat Kumar <bharat.nanavathula@gmail.com>
2025-08-08 00:52:39 +0530

    changed opp eye option

--- app/dashboard/teammanagement/page.jsx
+++ app/dashboard/teammanagement/page.jsx
@@ -691,7 +691,7 @@ export default function AdminPage() {
                     className="absolute inset-y-0 right-0 flex items-center pr-3 text-gray-500 hover:text-gray-700 cursor-pointer"
                     tabIndex={-1}
                   >
-                    {showPassword ? <EyeOff size={18} /> : <Eye size={18} />}
+                    {showPassword ? <Eye size={18} /> :  <EyeOff size={18} />}
                   </button>
                 </div>
                    </div>
